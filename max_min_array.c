eff070e57095a88daf6e7f211c2d1918a3b4bd52
swaroop002 <55097499+swaroop002@users.noreply.github.com>
2022-07-13 12:24:13 +0530

    Add files via upload

---
+++ max_min_array.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+void fill(int *a,int n){
+    for(int i=0;i<n;i++){
+        printf("Enter Element : ");
+        scanf("%d",(a+i));
+    }
+}
+void display(int *a,int n){
+    printf("\nArray Elements : ");
+    for(int i=0;i<n;i++){
+        printf("%d\t",*(a+i));
+    }
+}
+void max_min(int *a,int n){
+    int i=0,lg=0,sm;
+    lg=*(a+i);
+    while(i<n-1){
+        if(lg<*(a+(i+1))){
+            lg=*(a+(i+1));
+        }
+        i++;
+    }
+    printf("\nLargest element is : %d",lg);
+    i=0;
+    sm=*(a+i);
+    while(i<n-1){
+        if(sm>*(a+(i+1))){
+            sm=*(a+(i+1));
+        }
+        i++;
+    printf("\nSmallest element is : %d",sm);
+    }
+}
+
+void main()
+{
+    int *a;
+    int n;
+    printf("Enter array size : ");
+    scanf("%d",&n);
+    a=(int*)calloc(n,sizeof(int));
+    fill(a,n);
+    display(a,n);
+    max_min(a,n);
+}
