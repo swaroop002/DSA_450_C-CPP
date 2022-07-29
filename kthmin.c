f3c65b47effb0320f7367e27f261c4c9be921b79
swaroop002 <55097499+swaroop002@users.noreply.github.com>
2022-07-29 12:15:14 +0530

    Add files via upload

---
+++ kthmin.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void allocate(int *a,int len){
+    for (int i=0;i<len;i++){
+        printf("\nEnter Element :");
+        scanf("%d",(a+i));
+    }
+}
+void display(int *a,int len){
+    printf("\nArray Elements :");
+    for (int i=0;i<len;i++){
+        printf("\t%d",*(a+i));
+    }
+}
+int kthmin(int *a,int len,int k){
+    int i,j,small;
+    for (i = 0; i < k; i++) {
+      small=i;
+      for (j = i+1; j < len; j++) {
+        if(*(a+j)<*(a+small))
+          small=j;
+      }
+      int temp=*(a+small);
+      *(a+small)=*(a+i);
+      *(a+i)=temp;
+    }
+    return(*(a+(k-1)));
+
+}
+
+void main()
+{
+    int *ary;
+    int len,k;
+    printf("Enter Length Of Array : ");
+    scanf("%d",&len);
+    ary=(int*)calloc(len,sizeof(int));
+    allocate(ary,len);
+    printf("Enter K : ");
+    scanf("%d",&k);
+    display(ary,len);
+    printf("%dth smallest element is %d \n", k ,kthmin(ary,len,k));;
+
+}
